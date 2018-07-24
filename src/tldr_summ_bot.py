4748110c7da92086d3a34ebc024f1f61e58f4de1
franfj <franjrg94@gmail.com>
2018-07-24 22:55:01 +0200

    Develop percentage handling

--- src/tldr_summ_bot.py
+++ src/tldr_summ_bot.py
@@ -4,14 +4,32 @@ from text_summarizer import summarizer
 
 
 def start_message(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text="Text summarization bot. Done by Fran Rodrigo.")
+    bot.sendMessage(chat_id=update.message.chat_id, text='Text summarization bot. Done by Fran Rodrigo.')
+
+
+def percentage_message(bot, update):
+    # Set error message
+    error_msg = 'Incorrect value. Please send a floating point number in the range [0 - 1] (i.e. 0.5).'
+
+    if len(update.message.text) > 4:
+        try:
+            pct = float(update.message.text[5:].encode('utf-8'))
+            if 0.0 <= pct <= 1.0:
+                summarizer.percentage = pct
+                bot.sendMessage(chat_id=update.message.chat_id, text='Percentage setted :)')
+
+        except ValueError:
+                bot.sendMessage(chat_id=update.message.chat_id, text=error_msg)
+
+    else:
+        bot.sendMessage(chat_id=update.message.chat_id, text=error_msg)
 
 
 def tldr_message(bot, update):
     if len(update.message.text) > 5:
-        text = update.message.text[6:]
-        bot.sendMessage(chat_id=update.message.chat_id,
-                        text=summarizer.schematize(text))
+        text = update.message.text[6:].encode('utf-8')
+        summ = summarizer.schematize(text)
+        bot.sendMessage(chat_id=update.message.chat_id, text=summ)
 
 
 with open('token.txt') as f:
@@ -22,6 +40,9 @@ dispatcher = updater.dispatcher
 
 start_handler = CommandHandler('start', start_message)
 dispatcher.add_handler(start_handler)
+
+percentage_handler = CommandHandler('pct', percentage_message)
+dispatcher.add_handler(percentage_handler)
 
 tldr_handler = CommandHandler('tldr', tldr_message)
 dispatcher.add_handler(tldr_handler)
