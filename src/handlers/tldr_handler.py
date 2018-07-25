9a8391b3088a1ff2704010d5a0b69485b37dff37
franfj <franjrg94@gmail.com>
2018-07-25 15:21:17 +0200

    Develop URLs summarization

---
+++ src/handlers/tldr_handler.py
@@ -0,0 +1,35 @@
+import validators
+from text_summarizer import summarizer
+
+from tools import UrlGrabber
+
+
+class TldrHandler(object):
+    """
+    Class for handling /tldr messages
+    """
+
+    @staticmethod
+    def summarize_reply(update):
+        input_text = update.message.reply_to_message.text
+        text = TldrHandler.check_for_url(input_text)    # Search for url's
+        return summarizer.schematize(text)
+
+    @staticmethod
+    def tldr_message(bot, update):
+        # Summarize reply messages
+        if update.message.reply_to_message is not None:
+            bot.sendMessage(chat_id=update.message.chat_id, text=TldrHandler.summarize_reply(update))
+
+        # Summarize common messages
+        elif len(update.message.text) > 5:
+            input_text = update.message.text[6:].encode('utf-8')
+            text = TldrHandler.check_for_url(input_text)    # Search for url's
+            summ = summarizer.schematize(text)
+            bot.sendMessage(chat_id=update.message.chat_id, text=summ)
+
+    @staticmethod
+    def check_for_url(text):
+        if validators.url(text):
+            text = UrlGrabber.get_text_from_url(text)
+        return text
--- src/tldr_summ_bot.py
+++ src/tldr_summ_bot.py
@@ -1,43 +1,7 @@
 from telegram.ext import CommandHandler
 from telegram.ext import Updater
-from text_summarizer import summarizer
 
-
-def summarize_reply(bot, update):
-    return summarizer.schematize(update.message.reply_to_message.text)
-
-
-def start_message(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id, text='Text summarization bot. Done by Fran Rodrigo.')
-
-
-def percentage_message(bot, update):
-    # Set error message
-    error_msg = 'Incorrect value. Please send a floating point number in the range [0 - 1] (i.e. 0.5).'
-
-    if len(update.message.text) > 4:
-        try:
-            pct = float(update.message.text[5:].encode('utf-8'))
-            if 0.0 <= pct <= 1.0:
-                summarizer.percentage = pct
-                bot.sendMessage(chat_id=update.message.chat_id, text='Percentage setted :)')
-
-        except ValueError:
-            bot.sendMessage(chat_id=update.message.chat_id, text=error_msg)
-
-    else:
-        bot.sendMessage(chat_id=update.message.chat_id, text=error_msg)
-
-
-def tldr_message(bot, update):
-    if update.message.reply_to_message is not None:
-        bot.sendMessage(chat_id=update.message.chat_id, text=summarize_reply(bot, update))
-
-    if len(update.message.text) > 5:
-        text = update.message.text[6:].encode('utf-8')
-        summ = summarizer.schematize(text)
-        bot.sendMessage(chat_id=update.message.chat_id, text=summ)
-
+from handlers import StartHandler, PercentageHandler, TldrHandler
 
 with open('token.txt') as f:
     token = f.readlines()[0]
@@ -45,13 +9,13 @@ with open('token.txt') as f:
 updater = Updater(token=token)
 dispatcher = updater.dispatcher
 
-start_handler = CommandHandler('start', start_message)
+start_handler = CommandHandler('start', StartHandler.start_message)
 dispatcher.add_handler(start_handler)
 
-percentage_handler = CommandHandler('pct', percentage_message)
+percentage_handler = CommandHandler('pct', PercentageHandler.percentage_message)
 dispatcher.add_handler(percentage_handler)
 
-tldr_handler = CommandHandler('tldr', tldr_message)
+tldr_handler = CommandHandler('tldr', TldrHandler.tldr_message)
 dispatcher.add_handler(tldr_handler)
 
 updater.start_polling()
